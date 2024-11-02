f8c4e013f316cc93a536c448d9ba381ed40e8fc7
Tanmay Rao <tanmay.rao@hotelkeyapp.com>
2024-11-02 14:21:17 +0530

    Updated grovvy

--- jobs/nginx.groovy
+++ jobs/nginx.groovy
@@ -1,12 +1,19 @@
-import hudson.plugins.git.*;
-
-def scm = new GitSCM("git@github.com:TanmayRao7/JENKINS_HELM.git")
-scm.branches = [new BranchSpec("*/main")];
-
-def flowDefinition = new org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition(scm, "Jenkinsfile")
-
-def parent = Jenkins.instance
-def job = new org.jenkinsci.plugins.workflow.job.WorkflowJob(parent, "NGINX")
-job.definition = flowDefinition
-
-parent.reload()
+pipelineJob('example-scm-job') {
+    description('Pipeline job configured using Job DSL and SCM')
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url('https://github.com/TanmayRao7/nginx-helm.git') 
+                    }
+                    branch('main') 
+                }
+            }
+            scriptPath('Jenkinsfile')
+        }
+    }
+    logRotator {
+        numToKeep(1)
+    }
+}
